f1e39e68469d6b7735dd7e1a557bce73fa0f3743
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-04-20 15:39:50 +0200

    add cache functionality to PokeAPI client

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -12,6 +12,16 @@ func (c *Client) ListLocations(pageURL *string) (LocationsResponse, error) {
 		url = *pageURL
 	}
 
+	if val, ok := c.cache.Get(url); ok {
+		locationsResp := LocationsResponse{}
+		err := json.Unmarshal(val, &locationsResp)
+		if err != nil {
+			return LocationsResponse{}, err
+		}
+
+		return locationsResp, nil
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return LocationsResponse{}, err
@@ -23,16 +33,17 @@ func (c *Client) ListLocations(pageURL *string) (LocationsResponse, error) {
 	}
 	defer resp.Body.Close()
 
-	dat, err := io.ReadAll(resp.Body)
+	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return LocationsResponse{}, err
 	}
 
 	locationsResp := LocationsResponse{}
-	err = json.Unmarshal(dat, &locationsResp)
+	err = json.Unmarshal(data, &locationsResp)
 	if err != nil {
 		return LocationsResponse{}, err
 	}
 
+	c.cache.Add(url, data)
 	return locationsResp, nil
 }
